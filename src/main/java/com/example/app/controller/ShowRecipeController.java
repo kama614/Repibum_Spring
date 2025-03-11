9548001374045d535446bcd2a5bdd6d4baa6e2a4
kamada <kamada@example.com>
2025-03-11 15:38:41 +0900

    編集機能と削除機能つけた

--- src/main/java/com/example/app/controller/ShowRecipeController.java
+++ src/main/java/com/example/app/controller/ShowRecipeController.java
@@ -2,13 +2,18 @@ package com.example.app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.app.domain.Recipe;
 import com.example.app.service.RecipeService;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -39,6 +44,35 @@ public class ShowRecipeController {
 			throw new RuntimeException("レシピ詳細の取得中にエラーが発生しました。", e);
 		}
 	}
+
+	// レシピデータの編集
+	@PostMapping("/update{id}")
+	public String updatePost(
+			@PathVariable Integer id,
+			@Valid Recipe recipe,
+			Errors errors,
+			RedirectAttributes rd,
+			Model model) {
+		if (errors.hasErrors()) {
+			model.addAttribute("title", "レシピ情報の変更");
+			model.addAttribute("types", recipeService.getRecipeById(id));
+			return "save";
+		}
+		recipe.setId(id); //更新に必要なレシピID をセット
+		recipeService.updateRecipe(recipe);
+		rd.addFlashAttribute("statusMessage", "レシピ情報を更新しました。");
+		return "redirect:/show/{id}";
+	}
+
+	// レシピデータの削除
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable Integer id,
+			RedirectAttributes rd) {
+		recipeService.deleteRecipe(id);
+		rd.addFlashAttribute("statusMessage", "レシピを削除しました");
+		return "redirect:/recipe/list";
+	}
+
 }
 
 /*
